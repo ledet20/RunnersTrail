1af8b9180504aa7098fdf113ecf34de8d87782ec
ledet <ledet2016@gmail.com>
2018-01-08 18:13:37 -0600

    reverse geocoding to get users location

--- app/src/main/java/com/ledetawano/runnerstrail/MainActivity.java
+++ app/src/main/java/com/ledetawano/runnerstrail/MainActivity.java
@@ -3,6 +3,8 @@ package com.ledetawano.runnerstrail;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,6 +15,11 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,7 +52,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onLocationChanged(Location location) {
 
-                Log.i("location", location.toString());
+                // location translated into language of users set device
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+
+                    //store current address in list
+                    // code will run if the user initially doesnt have location services on but allows it when application runs
+                    List<Address> listAddress = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if(listAddress != null && listAddress.size() > 0) {
+
+                        // concatenate users full address if info is available
+                        String fullAddress = "";
+
+                        Toast.makeText(getApplicationContext(), listAddress.get(0).toString(), Toast.LENGTH_LONG).show();
+                        
+                    }
+
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                }
 
             }
 
@@ -78,6 +107,28 @@ public class MainActivity extends AppCompatActivity {
             } else {
 
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+                // if the app is launched and we can get the users location  we want to get their last known location
+
+                Location lastKnownlocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                Geocoder geo = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+
+                    List<Address> addressList = geo.getFromLocation(lastKnownlocation.getLatitude(), lastKnownlocation.getLongitude(), 1);
+
+                    if(addressList != null && addressList.size() > 0) {
+
+                        Toast.makeText(getApplicationContext(), addressList.get(0).toString(),Toast.LENGTH_LONG).show();
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // LatLng currentLocation = new LatLng(lastKnownlocation.getLatitude(), lastKnownlocation.getLongitude());
 
             }
 
